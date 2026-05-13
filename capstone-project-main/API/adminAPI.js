87c1aed4317a5e5e79be14c39e9e793b50517d53
keerthanreddy691 <keerthansankepally@gmail.com>
2026-05-13 23:43:43 +0530

    Refactor admin API routes and improve structure
    Refactor admin API routes for better organization and readability. Added comments to separate user and author functionalities.

--- capstone-project-main/API/adminAPI.js
+++ capstone-project-main/API/adminAPI.js
@@ -1,105 +1,195 @@
-import exp from 'express'
-import { userModel } from '../Models/userModel.js'
-import { verifyToken } from '../middleware/verifyToken.js'
+import exp from "express";
+import { userModel } from "../Models/userModel.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
-export const adminApp = exp.Router()
-
-// Get all USERS (only emails)
-adminApp.get("/users", verifyToken("ADMIN"), async (req, res) => {
-  try {
-    const users = await userModel.find({ role: "USER" }, { email: 1, _id: 1 })
-    res.status(200).json({ message: "users", payload: users })
-  } catch (err) {
-    res.status(500).json({ message: "error", error: err.message })
-  }
-})
-
-// Get all AUTHORS (only emails)
-adminApp.get("/authors", verifyToken("ADMIN"), async (req, res) => {
-  try {
-    const authors = await userModel.find({ role: "AUTHOR" }, { email: 1, _id: 1 })
-    res.status(200).json({ message: "authors", payload: authors })
-  } catch (err) {
-    res.status(500).json({ message: "error", error: err.message })
-  }
-})
+export const adminApp = exp.Router();
 
 
-// Block USER
-adminApp.put("/user/block/:id", verifyToken("ADMIN"), async (req, res) => {
-  try {
-    const user = await userModel.findByIdAndUpdate(
-      req.params.id,
-      { isUserActive: false },
-      { new: true }
-    )
+// ================= GET USERS =================
+adminApp.get(
+  "/users",
+  verifyToken("ADMIN"),
+  async (req, res) => {
+    try {
 
-    if (!user) {
-      return res.status(404).json({ message: "user not found" })
+      const users = await userModel.find(
+        { role: "USER" }
+      );
+
+      res.status(200).json({
+        message: "users",
+        payload: users,
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+        message: "error",
+        error: err.message,
+      });
     }
-
-    res.status(200).json({ message: "user blocked", payload: user })
-  } catch (err) {
-    res.status(500).json({ message: "error", error: err.message })
   }
-})
+);
 
 
-//Activate USER
-adminApp.put("/user/activate/:id", verifyToken("ADMIN"), async (req, res) => {
-  try {
-    const user = await userModel.findByIdAndUpdate(
-      req.params.id,
-      { isUserActive: true },
-      { new: true }
-    )
+// ================= GET AUTHORS =================
+adminApp.get(
+  "/authors",
+  verifyToken("ADMIN"),
+  async (req, res) => {
+    try {
 
-    if (!user) {
-      return res.status(404).json({ message: "user not found" })
+      const authors = await userModel.find(
+        { role: "AUTHOR" }
+      );
+
+      res.status(200).json({
+        message: "authors",
+        payload: authors,
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+        message: "error",
+        error: err.message,
+      });
     }
-
-    res.status(200).json({ message: "user activated", payload: user })
-  } catch (err) {
-    res.status(500).json({ message: "error", error: err.message })
   }
-})
+);
 
 
-// Block AUTHOR
-adminApp.put("/author/block/:id", verifyToken("ADMIN"), async (req, res) => {
-  try {
-    const author = await userModel.findOneAndUpdate(
-      { _id: req.params.id, role: "AUTHOR" },
-      { isUserActive: false },
-      { new: true }
-    )
+// ================= BLOCK USER =================
+adminApp.put(
+  "/user/block/:id",
+  verifyToken("ADMIN"),
+  async (req, res) => {
+    try {
 
-    if (!author) {
-      return res.status(404).json({ message: "author not found" })
+      const updatedUser =
+        await userModel.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: {
+              isUserActive: false,
+            },
+          },
+          { new: true }
+        );
+
+      res.status(200).json({
+        message: "user blocked",
+        payload: updatedUser,
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+        message: "error",
+        error: err.message,
+      });
     }
-
-    res.status(200).json({ message: "author blocked", payload: author })
-  } catch (err) {
-    res.status(500).json({ message: "error", error: err.message })
   }
-})
+);
 
 
-// Activate AUTHOR
-adminApp.put("/author/activate/:id", verifyToken("ADMIN"), async (req, res) => {
-  try {
-    const author = await userModel.findOneAndUpdate(
-      { _id: req.params.id, role: "AUTHOR" },
-      { isUserActive: true },
-      { new: true }
-    )
+// ================= ACTIVATE USER =================
+adminApp.put(
+  "/user/activate/:id",
+  verifyToken("ADMIN"),
+  async (req, res) => {
+    try {
 
-    if (!author) {
-      return res.status(404).json({ message: "author not found" })
+      const updatedUser =
+        await userModel.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: {
+              isUserActive: true,
+            },
+          },
+          { new: true }
+        );
+
+      res.status(200).json({
+        message: "user activated",
+        payload: updatedUser,
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+        message: "error",
+        error: err.message,
+      });
     }
-
-    res.status(200).json({ message: "author activated", payload: author })
-  } catch (err) {
-    res.status(500).json({ message: "error", error: err.message })
   }
-})
+);
+
+
+// ================= BLOCK AUTHOR =================
+adminApp.put(
+  "/author/block/:id",
+  verifyToken("ADMIN"),
+  async (req, res) => {
+    try {
+
+      const updatedAuthor =
+        await userModel.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: {
+              isUserActive: false,
+            },
+          },
+          { new: true }
+        );
+
+      res.status(200).json({
+        message: "author blocked",
+        payload: updatedAuthor,
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+        message: "error",
+        error: err.message,
+      });
+    }
+  }
+);
+
+
+// ================= ACTIVATE AUTHOR =================
+adminApp.put(
+  "/author/activate/:id",
+  verifyToken("ADMIN"),
+  async (req, res) => {
+    try {
+
+      const updatedAuthor =
+        await userModel.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: {
+              isUserActive: true,
+            },
+          },
+          { new: true }
+        );
+
+      res.status(200).json({
+        message: "author activated",
+        payload: updatedAuthor,
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+        message: "error",
+        error: err.message,
+      });
+    }
+  }
+);
