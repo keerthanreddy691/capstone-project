560054ef4b6c4ba63e4ae6831c1bb08aa3d7565b
keerthanreddy691 <keerthansankepally@gmail.com>
2026-05-20 09:11:03 +0530

    Refactor verifyToken middleware for improved clarity

--- capstone-project-main/middleware/verifyToken.js
+++ capstone-project-main/middleware/verifyToken.js
@@ -1,30 +1,29 @@
-import jwt from 'jsonwebtoken'
-import {config} from "dotenv"
-const {verify}=jwt
-config();
+export const verifyToken = (...allowedRoles) => {
+  return (req, res, next) => {
+    try {
+      console.log("Cookies:", req.cookies);
 
+      const token = req.cookies?.token;
 
-export const verifyToken=(...allowedRoles)=>{
-    return (req,res,next)=>{
-    try{
-    //get token from cookie
-    const token= req.cookies?.token
-    //check token existed or not
-    if(!token){
-        return res.status(401).json({message:"please login first"})
+      if (!token) {
+        console.log("No token found");
+        return res.status(401).json({ message: "please login first" });
+      }
+
+      const decodedToken = verify(token, process.env.SECRET_KEY);
+
+      console.log("Decoded Token:", decodedToken);
+
+      if (!allowedRoles.includes(decodedToken.role)) {
+        console.log("Role mismatch:", decodedToken.role);
+        return res.status(403).json({ message: "you are not authorized" });
+      }
+
+      req.user = decodedToken;
+      next();
+    } catch (err) {
+      console.log("JWT ERROR:", err.message);
+      return res.status(401).json({ message: "invalid token" });
     }
-    //validate token
-    let decodedToken=verify(token,process.env.SECRET_KEY)
-    //check the role is same as the role in decoded token
-    if(!allowedRoles.includes(decodedToken.role)){
-        return res.status(403).json({message:"you are not authorized"})
-    }
-
-    //add decoded token
-    req.user=decodedToken;
-    next()
-}catch(err){
-    res.status(401).json({message:"invalid token"})
-}
-
-}}
+  };
+};
